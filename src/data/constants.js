22b73aa5a73277812b2c5655fb8bb45ea3208d08
Anirudha1821 <udgirkaranirudha@gmail.com>
2024-10-13 23:13:13 +0530

    updated resume _added team features

--- src/data/constants.js
+++ src/data/constants.js
@@ -15,7 +15,7 @@ export const Bio = {
   github: "https://github.com/Anirudha1821",
   leetcode: "https://leetcode.com/u/Anirudha2107/",
   email:"mailto:udgirkaranirudha@gmail.com",
-  resume:"https://drive.google.com/file/d/1NiCxhKMHickj7lye6jbBp6Mtk0LZTbMN/view",
+  resume:"https://drive.google.com/file/d/1fLVh_s6wyTCdvOYAMajr-LJ73mBATGGF/view",
   linkedin: "https://www.linkedin.com/in/anirudha-udgirkar-13ab53244/",
   insta: "https://www.instagram.com/anirudha07_21/",
 };
